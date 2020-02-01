e43b56f9ffea8191cca2413392a3e7fd86257a81
Pranit Panda <pranitpanda2.0@gmail.com>
2020-02-01 11:45:15 -0800

    added new auto to reset encoders

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ResetEncoders.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ResetEncoders.java
@@ -2,15 +2,15 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
-@Autonomous(name = "small auto",group = "SkyStone")
-public class smallauto extends BaseAutonomous {
-
+@Autonomous(name = "ResetEncoders",group = "SkyStone")
+public class ResetEncoders extends BaseAutonomous {
     @Override
     public void runOpMode() throws InterruptedException {
         inithardware(false);
         waitForStart();
-        encoderMecanumDrive(0.5,1,1,0,1);
-        encoderMecanumDrive(0.5,30,1,1,0);
+        telemetry.addData("horizontal",robot.horizontalSlider.getCurrentPosition());
+
+
     }
 
 }
